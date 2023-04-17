6520181f869c0660cc189a2ddbd82522b37af2a4
Edmunds 221RDC037 <93928858+superetmunc@users.noreply.github.com>
2023-04-17 14:36:51 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,21 +1,15 @@
 def read_input():
-    source = input().strip()
-    if source == 'F':
-        with open('test.txt', 'r') as f:
-            pattern = f.readline().strip()
-            text = f.readline().strip()
-    elif source == 'I':
-        pattern = input().strip()
-        text = input().strip()
-    else:
-        print("Invalid input source")
-        exit()
+    
+    pattern = input().rstrip()
+    text = input().rstrip()
     return pattern, text
 
 def print_occurrences(output):
+    
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
+    
     occurrences = []
     p_len = len(pattern)
     t_len = len(text)
@@ -29,11 +23,13 @@ def get_occurrences(pattern, text):
             if text[i:i+p_len] == pattern:
                 occurrences.append(i)
         if i < t_len - p_len:
-            
+            # Update the hash value of the next window.
             t_hash = (t_hash - ord(text[i]) * (101 ** (p_len - 1))) % 10**9
             t_hash = (t_hash * 101 + ord(text[i+p_len])) % 10**9
 
+    
     return sorted(occurrences)
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
